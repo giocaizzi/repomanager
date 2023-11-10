96fdb95250de38207ac40bf8951c38e817506456
giocaizzi <giocaizzi@gmail.com>
2023-11-10 20:33:14 +0100

    wow

--- repomanager/app.py
+++ repomanager/app.py
@@ -1,15 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, redirect, url_for
 from decouple import config
+import logging
 
 from .github import GithubClient
 
+
+logging.basicConfig(level=logging.DEBUG)
+
 app = Flask(__name__)
+app.secret_key = config("SECRET_KEY")
 
 
 @app.route("/")
 def home():
-    user = GithubClient(config("GITHUB_TOKEN"))
-    return render_template("home.html", user=user)
+    if "login_type" in session and "login_input" in session:
+        return render_template("user.html", user=_login())
+    else:
+        return render_template("home.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 
 @app.route("/about")
@@ -17,5 +29,66 @@ def about():
     return render_template("about.html")
 
 
-if __name__ == "__main__":    
-    app.run(debug=True)
+@app.route("/auth", methods=["POST"])
+def auth():
+    session["login_type"] = request.form.get("login_type")
+    session["login_input"] = request.form.get("login_input")
+    app.logger.info("Login requested")
+    _login()
+    return redirect(url_for("user"))
+
+
+@app.route("/logout")
+def logout():
+    # Remove the keys from the session
+    session.pop("login_type", None)
+    session.pop("login_input", None)
+    app.logger.info("Logged out")
+    return redirect(url_for("home"))
+
+
+@app.route("/user")
+def user():
+    if "login_type" in session and "login_input" in session:
+        app.logger.info("User page requested with login")
+        return render_template("user.html", user=_login())
+    else:
+        app.logger.info("User page requested without login")
+        return redirect(url_for("home"))
+
+
+def _login():
+    """Returns a GithubClient object based on the login type and input.
+
+    Raises:
+        LoginError: If the login fails.
+        ValueError: If the login type is not recognized.
+        NotImplementedError: If the login type is not implemented.
+    """
+    if session.get("login_type") == "token":
+        app.logger.info("Logging in with token")
+        try:
+            return GithubClient(token=session.get("login_input"))
+        except Exception as e:
+            raise LoginError("Failed to login with token") from e
+
+    elif session.get("login_type") == "username":
+        app.logger.info("Logging in with username")
+        raise NotImplementedError("Only token login is currently supported.")
+    else:
+        raise ValueError("Unknown login type.")
+
+
+@app.errorhandler(Exception)
+def handle_exception(e):
+    # pass the error to the template
+    # returns the template with a 500 error
+    return render_template("error.html", error=e), 500
+
+
+class LoginError(Exception):
+    pass
+
+
+if __name__ == "__main__":
+    app.run()
