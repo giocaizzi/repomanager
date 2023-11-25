f47b0fe13b5ff71065be4a8713e1b131eabc1bf5
giocaizzi <giocaizzi@gmail.com>
2023-11-25 01:58:32 +0100

    format and lint

--- repomanager/app.py
+++ repomanager/app.py
@@ -1,13 +1,13 @@
 """repomanager app"""
 # Entry point for the application.
-from . import app    # For application discovery by the 'flask' command.
+from . import app  # For application discovery by the 'flask' command.
 from . import views  # For import side-effects of setting up routes.
 
 from .filters import is_none_filter, is_different_filter, icon_filter
 
-app.jinja_env.filters['icon'] = icon_filter
-app.jinja_env.filters['is_none'] = is_none_filter
-app.jinja_env.filters['is_different'] = is_different_filter
+app.jinja_env.filters["icon"] = icon_filter
+app.jinja_env.filters["is_none"] = is_none_filter
+app.jinja_env.filters["is_different"] = is_different_filter
 
 if __name__ == "__main__":
     app.run()
