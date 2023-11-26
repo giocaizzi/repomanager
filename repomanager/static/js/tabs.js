836b612849c488d719bfc9d48ed909eb0eefb7f3
giocaizzi <giocaizzi@gmail.com>
2023-11-26 01:43:12 +0100

    tabs and buttons

--- repomanager/static/js/tabs.js
+++ repomanager/static/js/tabs.js
@@ -1,8 +1,13 @@
-function openTab(tabName) {
-  var i;
-  var x = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+function openTab(evt, tabName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tabbutton");
+    for (i = 0; i < x.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
   }
-  document.getElementById(tabName).style.display = "block";  
-}
