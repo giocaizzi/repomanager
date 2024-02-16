544fd9a73b3012fb8e1dfcc143af8f26f608331b
giocaizzi <giocaizzi@gmail.com>
2024-02-16 21:59:05 +0100

    page layout

--- webapp/app/layout.js
+++ webapp/app/layout.js
@@ -1,5 +1,7 @@
 import "@components/globals.css";
 
+import Header from "@/components/Header/Header";
+
 export const metadata = {
   title: "repomanager",
   description: "Easily manage github repositories",
@@ -8,7 +10,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </head>
+      <body>
+        <Header />
+        <main>
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
---
+++ webapp/app/login/page.js
@@ -0,0 +1,9 @@
+export default function Login() {
+  return (
+    <div className="centered">
+      <>
+        <h2>login</h2>
+      </>
+    </div>
+  );
+}
