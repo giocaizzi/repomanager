eda3e3c99c9556e37676d225cfc81a78b8bd79c7
giocaizzi <giocaizzi@gmail.com>
2024-02-24 20:57:57 +0100

    fixed some react errors

--- webapp/components/Inputs/RadioInput/RadioInput.js
+++ webapp/components/Inputs/RadioInput/RadioInput.js
@@ -2,18 +2,21 @@ import PropTypes from 'prop-types';
 
 import './RadioInput.css';
 
-export default function RadioInput({label = "label", id="radio_id" , name="" , value=""}) {
+export default function RadioInput({ label = "label", id = "radio_id", name = "", value = "", checked = true }) {
     return (
         <div className='radioinput'>
-            <input type="radio" id={id} name={name} value={value} checked></input>
-            <label for={id}>{label}</label>
+            {checked ?
+                <input type="radio" id={id} name={name} value={value} defaultChecked></input>
+                :
+                <input type="radio" id={id} name={name} value={value}></input>}
+            <label htmlFor={id}>{label}</label>
         </div>
     );
 };
 
 RadioInput.propTypes = {
     label: PropTypes.string,
-    id : PropTypes.string,
-    name : PropTypes.string,
-    value : PropTypes.string
+    id: PropTypes.string,
+    name: PropTypes.string,
+    value: PropTypes.string
 };
--- webapp/components/Inputs/TextInput/TextInput.js
+++ webapp/components/Inputs/TextInput/TextInput.js
@@ -4,7 +4,7 @@ import './TextInput.css';
 export default function TextInput({ id = "login_input", name="", placeholder="insert" }) {
     return (
         <div className='textinput'>
-            <label for={id}>Enter your login information:</label>
+            <label htmlFor={id}>Enter your login information:</label>
             <input type="text" id={id} name={name} placeholder={placeholder}></input>
         </div>
     );
