c8b2ee9d1efd39ec69cc24a9050f83c6ee7ff307
giocaizzi <giocaizzi@gmail.com>
2024-02-25 15:00:14 +0100

    cors issues

--- webapp/lib/fetch.js
+++ webapp/lib/fetch.js
@@ -14,7 +14,13 @@ export async function fetchData(endpoint, method, body) {
                 method: method,
                 body: body,
                 headers: {
-                    "Content-Type": "application/json"
+                    "Content-Type": "application/json",
+                    // cors
+                    "Access-Control-Allow-Origin":  "*",
+                    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+                    "Access-Control-Allow-Headers":
+                        "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version",
+                    "Access-Control-Max-Age": "86400",
                 }
             }
         )
