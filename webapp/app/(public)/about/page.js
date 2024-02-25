ac86e579192735c811b1b72c7ad6fdf356b92633
giocaizzi <giocaizzi@gmail.com>
2024-02-25 22:00:47 +0100

    removed some templates

--- webapp/app/(public)/about/page.js
+++ webapp/app/(public)/about/page.js
@@ -2,7 +2,15 @@ export default function About() {
     return (
         <>
             <h1>About</h1>
-            <p>This is the about page</p>
+            <p>Repomanager is a tool to control GitHub repositories efficently.</p>
+            <h2>Links</h2>
+            <ul>
+                <li>
+                    For <b><em>Issues</em></b>, <b><em>Pull Requests</em></b> and the <b><em>source code</em></b> visit
+                    the
+                    <a href="https://github.com/giocaizzi/repomanager">GitHub Repository</a>
+                </li>
+            </ul>
         </>
     )
-}
+};
