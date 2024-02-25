4e4da7bbbeb78cce4d08704b93b8534ecc4a9ef2
giocaizzi <giocaizzi@gmail.com>
2024-02-25 15:01:16 +0100

    before cors

--- webapp/lib/fetch.js
+++ webapp/lib/fetch.js
@@ -15,12 +15,6 @@ export async function fetchData(endpoint, method, body) {
                 body: body,
                 headers: {
                     "Content-Type": "application/json",
-                    // cors
-                    "Access-Control-Allow-Origin":  "*",
-                    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-                    "Access-Control-Allow-Headers":
-                        "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version",
-                    "Access-Control-Max-Age": "86400",
                 }
             }
         )
