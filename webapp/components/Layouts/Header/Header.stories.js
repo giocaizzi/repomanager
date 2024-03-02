d3d3d79a36422b3fabbb0e009fc29606be83af25
giocaizzi <giocaizzi@gmail.com>
2024-03-02 19:18:51 +0100

    suspense

--- webapp/components/Layouts/Header/Header.stories.js
+++ webapp/components/Layouts/Header/Header.stories.js
@@ -1,8 +1,14 @@
 import Header from "./Header";
+import { Suspense } from 'react';
+
 
 export default {
     title: "Layouts/Header",
     component: Header,
 };
 
-export const Default = {}
+export const Default = () => (
+    <Suspense>
+        <Header />
+    </Suspense>
+);
