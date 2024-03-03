3ca30c96f811b141763e70d2e3dce7d08e87d9b6
giocaizzi <giocaizzi@gmail.com>
2024-03-03 01:24:24 +0100

    table columns chage size

--- webapp/components/Repositories/Repositories/Repositories.js
+++ webapp/components/Repositories/Repositories/Repositories.js
@@ -1,14 +1,15 @@
 import RepositoriesTable from '@/components/Repositories/Table/RepositoriesTable/RepositoriesTable'
 import Tabs from '@/components/Repositories/Tabs/Tabs'
+import styles from './Repositories.module.css'
 import PropTypes from 'prop-types'
 
 export default function Repositories({repositories}) {
     return (
-        <>
+        <div className={styles.repositories}>
         <Tabs />
         <br></br>
         <RepositoriesTable repositories={repositories}/>
-        </>
+        </div>
     )
     }
 
